5830c02d8c2024e24e3c854fc34727fa8297ee29
wu Ming <144977595+wuming0822@users.noreply.github.com>
2023-10-09 19:23:56 +0800

    Add files via upload

--- demo/work_School/4#_80.c
+++ demo/work_School/4#_80.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 #include <math.h>//包含头文件math.h
 int main ()
-{
-    int a = 0 , b = 0 , c = 0;//初始化
+{   
+    float a = 0 , b = 0 , c = 0;//初始化
     printf("请输入各三边长度:");
-    scanf("%d %d %d", &a ,&b ,&c);//进行输入赋值
-    int p = (a + b + c) / 2;
-    int S = 0;
+    again:
+    scanf("%f %f %f", &a ,&b ,&c);//进行输入赋值
+    if (a + b > c && a + c > b && c + b > a)
+    {
+    float p = (a + b + c) / 2;
+    float S = 0;
     S = sqrt (p * (p - a) * (p - b) * (p - c));//引用计算公式
-    printf("三角形面积为：%d\n", S);//进行输出
+    printf("三角形面积为：%.2f\n", S);//进行输出
+    }
+    else
+    {
+        printf("不能构成三角形,请重新输入\n");
+        goto again;
+    }
     return 0;
 }
