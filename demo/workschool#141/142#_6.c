c66a26d55991ebee1ae22afabeee0fa945b872f2
Wang <144977595+wuming0822@users.noreply.github.com>
2023-10-31 21:29:59 +0800

    Add files via upload

--- demo/workschool#141/142#_6.c
+++ demo/workschool#141/142#_6.c
@@ -1,17 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS 1//斐波那契数列
 #include <stdio.h>
-
-int main ()
+int main()
 {
-    int mon = 0 ;
-    scanf("%d",&mon);
-    int i = 0;
-    for(i = 0 ; i < 12; i++)
-    {
-        for()
-        {
-            
-        }
-    }
-
-    return 0;
-}
+	int a = 1;//第一个月兔子的数量
+	int b = 1;//第二个月兔子的数量
+	int c = 0;
+	int i = 0;
+	for (i = 1 ; i <= 12  ; i++)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+		printf("第%d月的兔子对数为%d\n",i, c);
+	}
+	printf("最后共有%d对兔子 \n",c);
+	return 0;
