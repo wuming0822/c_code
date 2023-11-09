277319ffb15128e8cffe3b60d6c1f0bbcc81d479
Wang <144977595+wuming0822@users.noreply.github.com>
2023-11-09 21:38:44 +0800

    Update 1_179#.c

--- demo/2023year_11:9/1_179#.c
+++ demo/2023year_11:9/1_179#.c
@@ -1,18 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
-int N! (int Num)//创建阶乘函数
+int N(int Num)//创建阶乘函数
 {
     int i = 0;
-    for (i = 0 ; i < Num ; i++)//循环
+    int tmp = 1;
+    for (i = 1; i <= Num; i++)//循环
     {
-        Num = Num * i;//阶乘
+        tmp = tmp * i;//阶乘
     }
-    return Num;
+    return tmp;
 }
-int main ()
+int main()
 {
-    int n = 0 , m = 0;
+    int n = 0, m = 0;
     printf("C");
-    scanf("%d%d",&n,&m);
-    printf("CMN = %d",N!(n) / N!(m) * N!(n-m));//调用函数
+    scanf("%d %d", &n, &m);
+    printf("CMN = %d", N(n) / (N(m) * N(n - m)));//调用函数
     return 0;
 }
