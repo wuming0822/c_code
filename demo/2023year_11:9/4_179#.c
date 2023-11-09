a14aa17209c9c19b31a38a0cf26d7d57bce8d3eb
Wang <144977595+wuming0822@users.noreply.github.com>
2023-11-09 20:24:18 +0800

    Update 4_179#.c

--- demo/2023year_11:9/4_179#.c
+++ demo/2023year_11:9/4_179#.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
-int gcb (int Num)//最大公约数
+int gcb (int A , int B)//最大公约数//欧几里得算法
 {
-    
+    if (A < B)
+    {
+        int tmp = A;
+        A = B;
+        B = tmp;
+    }
+    while(A % B != 0)
+    {
+        int and = A % B;
+        A = B;
+        B = and;
+    }
+    return B;//最小公约数
 }
 int lcm ()//最小公倍数
 {
@@ -12,6 +24,6 @@ int main ()
     int a = 0 , b = 0;
     printf("请输入两个数：");
     scanf("%d%d",&a,&b);
-
+    printf("%d和%d的公约数为：",a,b,gcb(a,b));
     return 0;
 }
